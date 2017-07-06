fix for cost derivatives

python/gps/algorithm/algorithm.py
@@ -133,7 +133,7 @@ class Algorithm(object):
             U = cur_data.get_U()
 
             # index last time step and sqeeze along first singleton dimesion
-            U_adv = np.squeeze(sample_prot.get_U()[-1:,:,:], axis=(0,))
+            U_adv = np.mean(sample_prot.get_U(), axis=0)
             # then add U
             # U += U_adv
 

python/gps/algorithm/cost/cost_action.py
@@ -44,19 +44,31 @@ class CostAction(Cost):
                 sample_u is now adversary's local u
                 sample_prot_u was the protagonist local u
                 we are maximizing with respect to v as in the IROS abstract
+                lv = 0.5 * sum(wu * u^2) - 2 * gamma * wu * v
+                lvv = 0.5 * sum(wu * u^2) - 2 * gamma * wu
             """
             sample_prot = kwargs['sample_prot']
             sample_prot_u = sample_prot.get_U()
 
-            # print('sample_prot_u: ', sample_prot_u.shape, ' | sample_u: ', sample_u.shape)
+            # print('sample_prot_u: ', sample_prot_u.shape, ' | sample_u: ', sample_u.shape) #('sample_prot_u: ', (100, 7), ' | sample_u: ', (100, 7))
             l = 0.5 * np.sum(self._hyperparams['wu'] * (sample_prot_u ** 2), axis=1) - \
                 self.gamma * np.sum( self._hyperparams['wu'] * (sample_u ** 2), axis=1)  # shape 100
             lv = 0.5 * np.sum(self._hyperparams['wu'] * (sample_prot_u ** 2), axis=1) - \
-                 (2 * self.gamma * np.sum(self._hyperparams['wu'] * sample_u, axis=1)) # 1st term shape (100,) 2nd term shape shape 7 x 7 due to 2nd term
+                 (2 * self.gamma * np.sum(self._hyperparams['wu'] * sample_u, axis=1)) # will be of shape (100,)
+            lv = np.expand_dims(lv, axis=1) #will be (100, 1)
+            lv = np.tile(lv, Du) #now (100, 7)
+
             lx = np.zeros((T, Dx))
-            lvv_temp = 0.5 * np.sum(self._hyperparams['wu'] * (sample_prot_u ** 2), axis=1) #shape (100,)
-            print('l shape: {}, lv shape: {}, lvv_temp: {}'.format(l.shape, lv.shape, lvv_temp.shape))
-            lvv = np.tile(np.diag(self._hyperparams['wu'] - 2 * self.gamma * self._hyperparams['wu']), [T, 1, 1]) # shape 100, 7, 7
+
+            # compute 2nd order control derivative
+            lvv_t1= 0.5 * np.sum(self._hyperparams['wu'] * (sample_prot_u ** 2), axis=1) # shape (100,)
+            lvv_t1 = np.expand_dims(lvv_t1, axis=1) # shape(100, 1)
+            lvv_t1 = np.tile(lvv_t1, 7)  # shape (100, 7)
+            lvv_t1 = np.expand_dims(lvv_t1, axis=2) # shape (100, 7, 1)
+            lvv_t1 = np.tile(lvv_t1, 7) # shape(100, 7, 7)
+            lvv_t2= np.tile(np.diag(2 * self.gamma * self._hyperparams['wu']), [T, 1, 1]) #shape (100, 7, 7)
+            # print('lvv_t1.shape: {},| lvv_t2.shape: {} ', lvv_t1.shape, lvv_t2.shape)
+            lvv = lvv_t1 - lvv_t2 # shape (100, 7, 7)
             lxx = np.zeros((T, Dx, Dx))
             lvx = np.zeros((T, Du, Dx))
 
@@ -70,8 +82,7 @@ class CostAction(Cost):
             #       2 *  self.gamma
             # lxx = np.zeros((T, Dx, Dx))
             # lux = np.zeros((T, Du, Dx))
-
-            return -l, -lx, -lu, -lxx, -luu, -lux
+            # return -l, -lx, -lu, -lxx, -luu, -lux
 
         else:
             os._exit("unknown mode. Cost Action Mode should either be protagonist or antagonist ")