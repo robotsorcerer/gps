fixed incorrect jacobian in action costs

python/gps/algorithm/cost/cost_action.py
@@ -35,6 +35,7 @@ class CostAction(Cost):
             lu = self._hyperparams['wu'] * sample_u
             lx = np.zeros((T, Dx))
             luu = np.tile(np.diag(self._hyperparams['wu']), [T, 1, 1])
+            print('luu shape: '.format(luu.shape))
             lxx = np.zeros((T, Dx, Dx))
             lux = np.zeros((T, Du, Dx))
 
@@ -64,12 +65,15 @@ class CostAction(Cost):
             # compute 2nd order control derivative
             lvv_t1= 0.5 * np.sum(self._hyperparams['wu'] * (sample_prot_u ** 2), axis=1) # shape (100,)
             lvv_t1 = np.expand_dims(lvv_t1, axis=1) # shape(100, 1)
-            lvv_t1 = np.tile(lvv_t1, 7)  # shape (100, 7)
-            lvv_t1 = np.expand_dims(lvv_t1, axis=2) # shape (100, 7, 1)
-            lvv_t1 = np.tile(lvv_t1, 7) # shape(100, 7, 7)
+            lvv_t1 = np.tile(lvv_t1, Du)  # shape (100, Du)
+            lvv_t1 = np.expand_dims(lvv_t1, axis=2) # shape (100, Du, 1)
+            lvv_t1 = np.tile(lvv_t1, Du) # shape(100, Du, Du)
             lvv_t2= np.tile(np.diag(2 * self.gamma * self._hyperparams['wu']), [T, 1, 1]) #shape (100, 7, 7)
-            # print('lvv_t1.shape: {},| lvv_t2.shape: {} ', lvv_t1.shape, lvv_t2.shape)
-            lvv = lvv_t1 - lvv_t2 # shape (100, 7, 7)
+            print('lvv_t1.shape: {},| lvv_t2.shape: {} ', lvv_t1.shape, lvv_t2.shape)
+            # if lvv_t2.shape[2] != lvv_t1.shape[2]:
+            #     lvv_t2 = np.empty_like(lvv_t1)
+            #     lvv_t2 = np.tile(np.diag(2 * self.gamma * np.ones(Dx)), [T, 1, 1])
+            lvv = lvv_t1 - lvv_t2 # shape (100, Du, Du)
             lxx = np.zeros((T, Dx, Dx))
             lvx = np.zeros((T, Du, Dx))
 

python/gps/algorithm/cost/cost_sum.py
@@ -1,6 +1,6 @@
 """ This file defines a cost sum of arbitrary other costs. """
 import copy, os
-
+import numpy as np
 from gps.algorithm.cost.config import COST_SUM
 from gps.algorithm.cost.cost import Cost
 
@@ -29,11 +29,17 @@ class CostSum(Cost):
         Args:
             sample:  A single sample
         """
-        self.mode = 'antagonist'
-        if 'sample_prot' in kwargs:
+        # print('hyperparams file: ', self._hyperparams)
+
+        self.mode = self._hyperparams['mode']
+        self.gamma = self._hyperparams['gamma']
+
+        if self.mode == 'antagonist': #'sample_prot' in kwargs:
             sample_prot = kwargs['sample_prot']
+            # print('evaluating cost in antagonist')
             l, lx, lu, lxx, luu, lux = self._costs[0].eval(sample, sample_prot=sample_prot)
         else:
+            # print('evaluating cost in protagonist')
             l, lx, lu, lxx, luu, lux = self._costs[0].eval(sample) #we are optimizing cost action
 
         # Compute weighted sum of each cost value and derivatives.
@@ -51,6 +57,10 @@ class CostSum(Cost):
             lx = lx + plx * weight
             lu = lu + plu * weight
             lxx = lxx + plxx * weight
+            # print('luu.shape: {},| pluu.shape: {} ', luu.shape, pluu.shape)
+            # print('costs: ', self._costs[i])
+            # if luu.shape[2] != pluu.shape[2]:
+            #     pluu = np.zeros(luu.shape)  # it's a tensor of zeros anyways
             luu = luu + pluu * weight
             lux = lux + plux * weight
         return l, lx, lu, lxx, luu, lux #don't negate here cause torque and fk costs are already negated

python/gps/gps_main.py
@@ -336,7 +336,7 @@ class GPSMain(object):
             pol_samples[cond][0] = self.agent.sample(
                 self.algorithm.policy_opt.policy, self._test_idx[cond],
                 verbose=verbose, save=False, noisy=False)
-        
+
         if self.closeloop or self.test:
             # print 'self.protag_algorithm: ', self.agent.sample(self.protag_algorithm.policy_opt.policy, self._test_idx[0],verbose=verbose, save=False, noisy=False)
             for cond in range(len(self._test_idx)):
@@ -517,7 +517,7 @@ def main():
         random.seed(seed)
         np.random.seed(seed)
 
-        self.closeloop = True
+        closeloop = True
 
         data_files_dir = exp_dir + 'data_files/'
         data_filenames = os.listdir(data_files_dir)
@@ -526,7 +526,7 @@ def main():
         current_algorithm = sorted(algorithm_filenames, reverse=True)[0]
         current_itr = int(current_algorithm[len(algorithm_prefix):len(algorithm_prefix)+2])
 
-        gps = GPSMain(hyperparams.config, args.quit, args.closeloop, test=test_policy_N)
+        gps = GPSMain(hyperparams.config, args.quit, closeloop, False)
         if hyperparams.config['gui_on']:
             run_gps = threading.Thread(
                 target=lambda: gps.run_cl(itr=current_itr, itr_load=resume_training_itr)